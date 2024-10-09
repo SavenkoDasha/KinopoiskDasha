e98368d5f1c724000d12d17e4ddd7bb3f79d996d
DashaSavenko <darya.savenko@letmecode.org>
2024-10-09 15:03:44 +0300

    kinopoisk-118 : refactored ViewBinding * Added ViewBinding

--- app/src/main/java/com/example/kinopoiskdasha/LoginFragment.kt
+++ app/src/main/java/com/example/kinopoiskdasha/LoginFragment.kt
@@ -1,7 +1,18 @@
 package com.example.kinopoiskdasha
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.kinopoiskdasha.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+    private val viewBinding: FragmentLoginBinding by viewBinding(FragmentLoginBinding::bind)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(viewBinding) {
+            loginBtn
+        }
+    }
 }
