3e3d0b787e37662396c3f8bca38dd96ef4845195
Dasha Savenko <darya.savenko@letmecode.org>
2024-10-14 17:42:18 +0300

    kinopoisk-127 : added coroutine * replace coroutine in viewModel

--- app/src/main/java/com/example/kinopoiskdasha/ui/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/example/kinopoiskdasha/ui/screens/login/LoginViewModel.kt
@@ -3,11 +3,18 @@ package com.example.kinopoiskdasha.ui.screens.login
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.kinopoiskdasha.data.Provider
+import com.example.kinopoiskdasha.data.dto.UserData
+import com.example.kinopoiskdasha.data.Provider
+import com.example.kinopoiskdasha.data.dto.UserData
+import com.example.kinopoiskdasha.data.Provider
 import com.example.kinopoiskdasha.data.UserDataSource
 import com.example.kinopoiskdasha.data.dto.UserData
 import com.example.kinopoiskdasha.ui.screens.login.LoginEvent.OnEmailChanged
 import com.example.kinopoiskdasha.ui.screens.login.LoginEvent.OnLoginClicked
 import com.example.kinopoiskdasha.ui.screens.login.LoginEvent.OnPasswordChanged
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -17,11 +24,14 @@ import kotlinx.coroutines.launch
 data class LoginUiState(
     val emailValue: String = "",
     val passwordValue: String = "",
+    var isButtonEnabled: Boolean = false,
 )
 
 class LoginViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(LoginUiState())
     val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
+
+    private var emailChangeJob: Job? = null
 
     fun handleEvent(event: LoginEvent) {
         when (event) {
@@ -32,8 +42,13 @@ class LoginViewModel : ViewModel() {
     }
 
     private fun changeEmail(new: String) {
-        viewModelScope.launch {
-            _uiState.update { it.copy(emailValue = new) }
+        emailChangeJob?.cancel()
+        _uiState.update { it.copy(emailValue = new) }
+        emailChangeJob = viewModelScope.launch {
+            _uiState.update {
+                delay(300L)
+                it.copy(isButtonEnabled = new.length >= 3)
+            }
         }
     }
 
@@ -45,8 +60,10 @@ class LoginViewModel : ViewModel() {
 
     private fun loginClicked() {
         Provider.dataSource.updateUser(
-            UserData(email = uiState.value.emailValue,
-            password = uiState.value.passwordValue)
+            UserData(
+                email = uiState.value.emailValue,
+                password = uiState.value.passwordValue
+            )
         )
     }
 }
