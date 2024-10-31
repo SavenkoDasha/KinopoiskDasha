4750e84043664b585208fd15e447b846712bbc5c
DashaSavenko <darya.savenko@letmecode.org>
2024-10-31 13:26:10 +0300

    kinopoisk-169 : changed movieDto * added interceptor * renamed dto * added movie data source * added retrofit2

---
+++ app/src/main/java/com/example/kinopoiskdasha/data/dto/MovieDto.kt
@@ -0,0 +1,23 @@
+package com.example.kinopoiskdasha.data.dto
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class MovieDto(
+    @Json(name = "nameOriginal")
+    val nameOriginal: String? = null,
+    @Json(name = "description")
+    val description: String? = null,
+    @Json(name = "countries")
+    val countries: String? = null,
+    @Json(name = "genres")
+    val genres: String? = null,
+    @Json(name = "startYear")
+    val startYear: String? = null,
+    @Json(name = "endYear")
+    val endYear: String? = null,
+    @Json(name = "ratingKinopoisk")
+    val ratingKinopoisk: Double? = null,
+    @Json(name = "imageUrl")
+    val imageurl: String? = null,
+)
