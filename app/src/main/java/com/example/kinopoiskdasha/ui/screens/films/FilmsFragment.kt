2f9ce0078b74609d235e7251a79eb659120019b8
DashaSavenko <darya.savenko@letmecode.org>
2024-11-14 17:08:15 +0300

    kinopoisk-168 : added movieui * added diffutil * added image placeholder * added dependencies for glide * implemented viewholder * added filmsAdapter

--- app/src/main/java/com/example/kinopoiskdasha/ui/screens/films/FilmsFragment.kt
+++ app/src/main/java/com/example/kinopoiskdasha/ui/screens/films/FilmsFragment.kt
@@ -1,14 +1,15 @@
 package com.example.kinopoiskdasha.ui.screens.films
-
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.kinopoiskdasha.R
 import com.example.kinopoiskdasha.databinding.FragmentFilmsBinding
+import com.example.kinopoiskdasha.ui.KinopoiskApp
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.launch
 
@@ -18,18 +19,26 @@ class FilmsFragment : Fragment(R.layout.fragment_films) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         lifecycleScope.launch {
             viewModel.labels.consumeEach(::handleLabels)
         }
 
+        val filmsAdapter = FilmsAdapter()
         with(viewBinding) {
             logoutBtn.setOnClickListener {
-                viewModel.handleEvent(FilmsEvent.onLogOutClicked)
+                 viewModel.handleEvent(FilmsEvent.onLogOutClicked)
+            }
+            filmsRecyclerView.layoutManager =
+                LinearLayoutManager(KinopoiskApp.applicationContext())
+            filmsRecyclerView.adapter = filmsAdapter
+        }
+
+        lifecycleScope.launch {
+            viewModel.uiState.collect { state ->
+                filmsAdapter.saveData(state.films)
             }
         }
     }
-
     private fun handleLabels(label: FilmsLabel) {
         when (label) {
             FilmsLabel.OnNavigateToLogin -> {
