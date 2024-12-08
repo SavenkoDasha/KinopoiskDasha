9f156ee8ae4164e3cf5f9f761c1f078dee36d8bc
SavenkoDasha <darya.savenko@letmecode.org>
2024-12-08 23:00:21 +0300

    kinopoisk-film_section : fixed login, api-key and added new viewholder

--- app/src/main/java/com/example/kinopoiskdasha/ui/screens/films/FilmsAdapter.kt
+++ app/src/main/java/com/example/kinopoiskdasha/ui/screens/films/FilmsAdapter.kt
@@ -3,7 +3,6 @@ package com.example.kinopoiskdasha.ui.screens.films
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +10,7 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
 import com.example.kinopoiskdasha.R
 import com.example.kinopoiskdasha.databinding.FilmsItemBinding
+import com.example.kinopoiskdasha.databinding.YearItemBinding
 import com.example.kinopoiskdasha.ui.model.ListItem
 import com.example.kinopoiskdasha.ui.model.MovieUi
 import com.example.kinopoiskdasha.ui.model.YearItem
@@ -35,8 +35,41 @@ class FilmsAdapter : RecyclerView.Adapter<FilmsAdapter.ViewHolder>() {
 
     private val dataSet = AsyncListDiffer(this, diffUtil)
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val binding: FilmsItemBinding by viewBinding(FilmsItemBinding::bind)
+
+    sealed class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        abstract fun bind(item: ListItem)
+
+        class YearViewHolder(view: View) : ViewHolder(view) {
+            private val binding: YearItemBinding by viewBinding(YearItemBinding::bind)
+
+            override fun bind(item: ListItem) {
+                if (item !is YearItem) return
+
+                with(binding) {
+                    year.text = item.year
+                }
+            }
+        }
+
+        class MovieViewHolder(view: View) : ViewHolder(view) {
+            private val binding: FilmsItemBinding by viewBinding(FilmsItemBinding::bind)
+
+            override fun bind(item: ListItem) {
+                if (item !is MovieUi) return
+
+                with(binding) {
+                    Glide.with(root.context)
+                        .load(item.imageUrl)
+                        .placeholder(R.drawable.baseline_downloading_24)
+                        .into(ivFilm)
+                    tvFilmName.text = item.nameOriginal
+                    tvFilmGenres.text = item.genres
+                    tvFilmYearAndCountry.text = item.filmYearAndCountry
+                    tvFilmRating.text = item.ratingKinopoisk
+                }
+            }
+        }
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -52,36 +85,21 @@ class FilmsAdapter : RecyclerView.Adapter<FilmsAdapter.ViewHolder>() {
                 .inflate(R.layout.films_item, viewGroup, false)
 
             YEAR_ITEM -> LayoutInflater.from(viewGroup.context)
-                .inflate(Button.generateViewId(), viewGroup, false)
+                .inflate(R.layout.year_item, viewGroup, false)
 
             else -> throw IllegalStateException("Incorrect item's type")
         }
 
-        return ViewHolder(view)
+        return when (viewType) {
+            MOVIE_ITEM -> ViewHolder.MovieViewHolder(view)
+            YEAR_ITEM -> ViewHolder.YearViewHolder(view)
+            else -> throw IllegalStateException("Incorrect item's type")
+        }
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val type = getItemViewType(position)
-        when (type) {
-            MOVIE_ITEM -> {
-                with(viewHolder.binding) {
-                    Glide.with(root.context)
-                        .load(imageUrl)
-                        .placeholder(R.drawable.baseline_downloading_24)
-                        .into(ivFilm)
-                    tvFilmName.text = nameOriginal
-                    tvFilmGenres.text = genres
-                    tvFilmYearAndCountry.text = filmYearAndCountry
-                    tvFilmRating.text = ratingKinopoisk
-                }
-            }
-            YEAR_ITEM -> {
-
-            }
-            else -> {
-                throw IllegalStateException("Wrong type")
-            }
-        }
+        val item = dataSet.currentList[position]
+        viewHolder.bind(item)
     }
 
     override fun getItemCount() = dataSet.currentList.size
